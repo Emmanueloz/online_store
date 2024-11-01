1e6dc70ff3735f4884b67cfad391f96591b016cf
EmmanuelOZ <davidozuna84@hotmail.com>
2024-11-01 01:07:50 -0600

    login de usuario

--- app/features/auth/routes.py
+++ app/features/auth/routes.py
@@ -4,19 +4,41 @@ from app.features.auth.model import User
 from app.auth.user import UserLogin
 from app.auth.roles import Roles
 from app.database import db
-from flask_login import login_user, logout_user
+from flask_login import login_user, logout_user, current_user
 from sqlalchemy.exc import IntegrityError
-
+from werkzeug.security import generate_password_hash, check_password_hash
 auth_bp = Blueprint('auth', __name__, template_folder='templates')
 
 
 @auth_bp.get('/login/')
 def login():
-    return render_template('login.jinja2')
+    if current_user.is_authenticated:
+        return redirect(url_for('home.index'))
+    context = {'form': LoginForm()}
+    return render_template('login.jinja2', **context)
+
+
+@auth_bp.post('/login/')
+def login_post():
+    form: LoginForm = LoginForm()
+    context = {'form': form}
+
+    if not form.validate_on_submit():
+        return render_template('login.jinja2', **context)
+
+    user: User = User.query.filter_by(username=form.username.data).first()
+    if not user or not check_password_hash(user.password, form.password.data):
+        flash('Usuario no encontrado', 'error')
+        return render_template('login.jinja2', **context)
+
+    login_user(UserLogin(user.id, user.username, user.email, user.password))
+    return redirect(url_for('home.index'))
 
 
 @auth_bp.get('/register/')
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home.index'))
     context = {'form': RegisterForm()}
     return render_template('register.jinja2', **context)
 
@@ -32,7 +54,8 @@ def register_post():
         return render_template('register.jinja2', **context)
 
     try:
-        user = User(form.username.data, form.email.data, form.password.data,
+        password_hash = generate_password_hash(form.password.data)
+        user = User(form.username.data, form.email.data, password_hash,
                     Roles.CLIENTE)
         db.session.add(user)
         db.session.commit()
@@ -45,3 +68,9 @@ def register_post():
         return render_template('register.jinja2', **context)
     except Exception as e:
         return render_template('error.jinja2')
+
+
+@auth_bp.get('/logout/')
+def logout():
+    logout_user()
+    return redirect(url_for('home.index'))
