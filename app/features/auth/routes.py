3609afdb5fe055840d1619296779bf0a44097e65
EmmanuelOZ <davidozuna84@hotmail.com>
2024-11-01 00:50:50 -0600

    registrar usuario

--- app/features/auth/routes.py
+++ app/features/auth/routes.py
@@ -1,5 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, flash
 from app.features.auth.form import LoginForm, RegisterForm
+from app.features.auth.model import User
+from app.auth.user import UserLogin
+from app.auth.roles import Roles
+from app.database import db
+from flask_login import login_user, logout_user
+from sqlalchemy.exc import IntegrityError
 
 auth_bp = Blueprint('auth', __name__, template_folder='templates')
 
@@ -20,10 +26,22 @@ def register_post():
 
     form: RegisterForm = RegisterForm()
 
-    if form.validate_on_submit():
-        print(form.data)
+    context = {'form': form}
 
-        return 'Registrado'
-    else:
-        print(form.errors)
-        return 'Error'
+    if not form.validate_on_submit():
+        return render_template('register.jinja2', **context)
+
+    try:
+        user = User(form.username.data, form.email.data, form.password.data,
+                    Roles.CLIENTE)
+        db.session.add(user)
+        db.session.commit()
+        login_user(UserLogin(user.id, user.username,
+                   user.email, user.password))
+
+        return redirect(url_for('home.index'))
+    except IntegrityError:
+        flash('Usuario ya registrado', 'error')
+        return render_template('register.jinja2', **context)
+    except Exception as e:
+        return render_template('error.jinja2')
