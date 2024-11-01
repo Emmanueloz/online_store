3314f04da3d8606567b4aa18d80e40fdff1bd0d9
EmmanuelOZ <davidozuna84@hotmail.com>
2024-11-01 17:22:36 -0600

    enlistar productos al usuario comun

--- app/features/home/routes.py
+++ app/features/home/routes.py
@@ -1,8 +1,30 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, flash
+from app.auth.role_authenticate import role_authenticate
+from app.auth.roles import Roles
+from app.features.products.model import Product
 
 home_bp = Blueprint('home', __name__, template_folder='templates')
 
 
 @home_bp.get('/')
 def index():
-    return render_template('index.jinja2')
+    context = {
+        'products': Product.query.all()
+    }
+    return render_template('index.jinja2', **context)
+
+
+@home_bp.get('/category/<path:path>/')
+def product_category(path):
+
+    if path not in ['electrónicos', 'ropa', 'comida', 'juguetes', 'deportes', 'libros', 'música', 'computadoras',
+                    'videojuego', 'otros']:
+        flash("Categoría no encontrada", 'error')
+        return redirect(url_for('home.index'))
+
+    context = {
+        'products': Product.query.filter(Product.category.like(path)).all(),
+        'category': path
+    }
+
+    return render_template('index.jinja2', **context)
