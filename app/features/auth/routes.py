cde3e0dcdd24b2e15b82ddc9d976379ba96aec4b
EmmanuelOZ <davidozuna84@hotmail.com>
2024-11-01 23:29:27 -0600

    actualizar datos del usuario

--- app/features/auth/routes.py
+++ app/features/auth/routes.py
@@ -117,3 +117,43 @@ def get_user():
     }
 
     return render_template('user.jinja2', **context)
+
+
+@auth_bp.post('/user/')
+def update_user():
+    user: User = User.query.filter_by(id=current_user.id).first()
+    if not user:
+        flash('Usuario no encontrado', 'error')
+        return redirect(url_for('home.index'))
+
+    form_user: UserForm = UserForm()
+    form_direction: DirectionForm = DirectionForm()
+
+    if form_user.validate_on_submit():
+        user.username = form_user.username.data
+        user.email = form_user.email.data
+        user.password = generate_password_hash(form_user.password.data)
+        db.session.commit()
+        flash('Datos actualizados', 'success')
+        return redirect(url_for('auth.get_user'))
+
+    if form_direction.validate_on_submit():
+        user.street = form_direction.street.data
+        user.number = form_direction.number.data
+        user.city = form_direction.city.data
+        user.state = form_direction.state.data
+        user.zip_code = form_direction.zip_code.data
+        user.country = form_direction.country.data
+        user.neighborhood = form_direction.neighborhood.data
+        user.phone = form_direction.phone.data
+        db.session.commit()
+        flash('Datos actualizados', 'success')
+        return redirect(url_for('auth.get_user'))
+
+    context = {
+        'user': user,
+        'form_user': form_user,
+        'form_direction': form_direction
+    }
+
+    return render_template('user.jinja2', **context)
