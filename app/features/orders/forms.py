0eeb12ca414371887ad9e4fab7ca55c36ebf9486
EmmanuelOZ <davidozuna84@hotmail.com>
2024-11-01 22:31:10 -0600

    idioma de la pagina en es_MX

--- app/features/orders/forms.py
+++ app/features/orders/forms.py
@@ -7,5 +7,5 @@ from datetime import date
 class OrderForm(FlaskForm):
     state = SelectField('Estado', choices=[
                        ('pending', 'Pendiente'), ('sent', 'Enviado'), ('delivered', 'Entregado')])
-    delivery_date = DateField('Fecha de entrega', format='%Y-%m-%d',
+    delivery_date = DateField('Fecha de entrega',
                               validators=[DataRequired()], render_kw={"min": date.today().isoformat()})
